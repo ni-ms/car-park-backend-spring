71a90fcf374c5ed76166cf528482356a8dc043ea
nikhilmsharma <nikmsharma@gmail.com>
2024-11-09 14:37:27 +0530

    Added User Service.

--- src/main/java/com/carparkspring/demo/service/UserService.java
+++ src/main/java/com/carparkspring/demo/service/UserService.java
@@ -1,6 +1,8 @@
 package com.carparkspring.demo.service;
 
 import com.carparkspring.demo.model.AppUser;
+import com.carparkspring.demo.model.AppUserData;
+import com.carparkspring.demo.repository.AppUserDataRepository;
 import com.carparkspring.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,12 +14,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private AppUserDataRepository appUserDataRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -32,7 +38,50 @@ public class UserService implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(appUser.getUsername(), appUser.getPassword(), authorities);
     }
 
-    public String addUser(AppUser userInfo) {
-        return null;
+    public List<AppUser> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<AppUser> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public AppUser saveUser(AppUser appUser) {
+        return userRepository.save(appUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public AppUserData getUserData(Long userId) {
+        return appUserDataRepository.findByAppUserId(userId);
+    }
+
+    public AppUserData saveUserData(AppUserData appUserData) {
+        return appUserDataRepository.save(appUserData);
+    }
+
+    public double getAccountBalance(Long userId) {
+        AppUserData appUserData = appUserDataRepository.findByAppUserId(userId);
+        return appUserData != null ? appUserData.getAccountBalance() : 0.0;
+    }
+
+    public void updateUsername(Long userId, String newUsername) {
+        Optional<AppUser> appUserOptional = userRepository.findById(userId);
+        if (appUserOptional.isPresent()) {
+            AppUser appUser = appUserOptional.get();
+            appUser.setUsername(newUsername);
+            userRepository.save(appUser);
+        }
+    }
+
+    public void updatePassword(Long userId, String newPassword) {
+        Optional<AppUser> appUserOptional = userRepository.findById(userId);
+        if (appUserOptional.isPresent()) {
+            AppUser appUser = appUserOptional.get();
+            appUser.setPassword(newPassword);
+            userRepository.save(appUser);
+        }
     }
 }
